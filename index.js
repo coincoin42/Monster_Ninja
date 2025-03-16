502629cfae13d4944b1825e7009104bf47f07f7e
coincoin42 <romain.coin42270@outlook.fr>
2025-03-16 20:00:05 +0100

    correction bug module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three';
+
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader'; 
 
@@ -8,7 +8,7 @@ import { ARButton } from './node_modules/three/examples/jsm/webxr/ARButton.js';
 
 
 import { FontLoader } from './node_modules/three/examples/jsm/loaders/FontLoader.js';
-
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js';
 
 const loader = new GLTFLoader();
 
