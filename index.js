b25fecb84760d209cd5b68120d0a148fa81b4ff7
coincoin42 <romain.coin42270@outlook.fr>
2025-03-16 13:13:06 +0100

    correction bug module

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import * as THREE from './node_modules/three';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader'; 
+//import * as THREE from './node_modules/three';
+//import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+//import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader'; 
 
 
 
-import { ARButton } from './node_modules/three/examples/jsm/webxr/ARButton.js';
+//import { ARButton } from './node_modules/three/examples/jsm/webxr/ARButton.js';
 
 
-import { FontLoader } from './node_modules/three/examples/jsm/loaders/FontLoader.js';
+//import { FontLoader } from './node_modules/three/examples/jsm/loaders/FontLoader.js';
 
 
 const loader = new GLTFLoader();
