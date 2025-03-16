1d7bf63362577b05968216f846acc900fe71335e
coincoin42 <130739209+coincoin42@users.noreply.github.com>
2025-03-16 20:11:27 +0100

    il manquait le js au module gltf loader

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'; 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'; 
 
 
 
